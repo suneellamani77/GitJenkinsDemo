dcb632e5180b39fa421e4d39c301affb9f9f4e84
sramappa <suneel.ramappalamani@sgdigital.com>
2020-01-17 20:34:19 +0530

    added the junit test case and gitignore file

---
+++ src/test/java/com/sg/ngs/EhCacheTest.java
@@ -0,0 +1,18 @@
+package com.sg.ngs;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EhCacheTest {
+	
+	@Test
+	public void addition() {
+		int a = 7;
+		int b = 8;
+		int c = a + b;
+		Assert.assertEquals("both are not equal", 16, c);
+	}
+	
+	
+
+}
